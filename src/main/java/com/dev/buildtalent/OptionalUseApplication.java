4467e0062da0aa2f92f2314a520724ee1dfb4be5
Devendra <devgit2016@gmail.com>
2023-11-13 17:04:08 +0530

    initial commit

---
+++ src/main/java/com/dev/buildtalent/OptionalUseApplication.java
@@ -0,0 +1,13 @@
+package com.dev.buildtalent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionalUseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptionalUseApplication.class, args);
+	}
+
+}
